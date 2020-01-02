a12690c9ccc587d0779db038a418bf6f651ed8a7
Jesús Miguel Cruz Cana <jesuscc1993@gmail.com>
2020-01-02 23:50:26 +0100

    Added hotkeys for event, guild wars and repeat quest options.

--- src/background/background.js
+++ src/background/background.js
@@ -1,6 +1,12 @@
-const { storage, tabs } = chrome;
+const { commands, storage, tabs } = chrome;
 
 const targetDomain = 'game.granbluefantasy.jp';
+
+const URL_KEYS = {
+  EVENT: 'event',
+  GUILD_WARS: 'guildWars',
+  LAST_QUEST: 'lastQuest',
+};
 
 const initialize = () => {
   storage.sync.get(['settings'], ({ settings }) => {
@@ -8,7 +14,9 @@ const initialize = () => {
       fetch('../../assets/data/defaultBookmarkSettings.json')
         .then(response => response.json())
         .then(defaultBookmarkSettings => {
-          storage.sync.set({ settings: { bookmarks: defaultBookmarkSettings } });
+          storage.sync.set({
+            settings: { bookmarks: defaultBookmarkSettings },
+          });
         });
     }
 
@@ -16,31 +24,68 @@ const initialize = () => {
       if (changeInfo.status == 'complete' && url.includes(targetDomain)) {
         if (url.includes('/supporter/')) {
           // quests
-          storage.sync.set({ lastQuest: url });
+          const key = URL_KEYS.LAST_QUEST;
+          storage.sync.set({ [key]: url });
         } else if (url.includes('/#event/teamraid')) {
           // guild wars
-          storage.sync.get(['guildWars'], response => {
-            const currentUrl = response.guildWars;
+          const key = URL_KEYS.GUILD_WARS;
+          storage.sync.get([key], response => {
+            const currentUrl = response[key];
             if (!url.includes(currentUrl)) {
-              storage.sync.set({ guildWars: url });
+              storage.sync.set({ [key]: url });
             }
           });
         } else if (isAnySubstringIncluded(url, ['/#event', '/#limited'])) {
           // events
-          storage.sync.get(['event'], response => {
-            const currentUrl = response.event;
+          const key = URL_KEYS.EVENT;
+          storage.sync.get([key], response => {
+            const currentUrl = response[key];
             if (!url.includes(currentUrl)) {
-              storage.sync.set({ event: url });
+              storage.sync.set({ [key]: url });
             }
           });
         }
       }
     });
+
+    commands.onCommand.addListener(command => {
+      const action = {
+        'open-event': openEvent,
+        'open-guild-wars': openGuildWars,
+        'repeat-quest': repeatQuest,
+      }[command];
+      action && action();
+    });
+  });
+};
+
+const openEvent = () => {
+  openStoredUrl(URL_KEYS.EVENT);
+};
+const openGuildWars = () => {
+  openStoredUrl(URL_KEYS.GUILD_WARS);
+};
+const repeatQuest = () => {
+  openStoredUrl(URL_KEYS.LAST_QUEST);
+};
+
+const openStoredUrl = key => {
+  storage.sync.get([key], response => {
+    const url = response[key];
+    if (url) {
+      tabs.update({ active: true, url });
+    } else {
+      alert('You need to enter once first to store it.');
+    }
   });
 };
 
 const isAnySubstringIncluded = (string, substrings) => {
-  return substrings.map(substring => string.includes(substring)).filter(included => included).length > 0;
+  return (
+    substrings
+      .map(substring => string.includes(substring))
+      .filter(included => included).length > 0
+  );
 };
 
 initialize();
