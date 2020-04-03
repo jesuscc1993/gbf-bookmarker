933d1c2b4249babae299a6a593c36e9b774e384e
Jesús Miguel Cruz Cana <jesuscc1993@gmail.com>
2020-04-03 21:12:39 +0200

    Fixed duplicate vendor folder

--- src/vendor/shared/file.utils.js
+++
@@ -1,5 +0,0 @@
-export const downloadFile = (content, fileName, type) => {
-  const file = new Blob([content], { type });
-  const href = URL.createObjectURL(file);
-  jQuery(`<a href="${href}" download="${fileName}">`)[0].click();
-};
--- src/vendor/src/shared/settings.utils.js
+++
@@ -1,6 +0,0 @@
-export const getSortedBookmarks = (bookmarks, settings) => {
-  return new Set([
-    ...((settings && settings.bookmarksOrder) || []),
-    ...Object.keys(bookmarks),
-  ]);
-};
