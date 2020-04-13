b6cc412fd5d0b3c64aa4867b3a8d80ed7870177a
Jesús Miguel Cruz Cana <jesuscc1993@gmail.com>
2020-04-13 14:50:54 +0200

    Removed unexpected src duplicate in vendor

--- src/vendor/storage/styles.storage.js
+++
@@ -1,9 +0,0 @@
-import { getFromStorage, setToStorage } from './storage.js';
-
-export const loadStyles = () => {
-  return getFromStorage(['styles']).then(response => response.styles);
-};
-
-export const storeStyles = styles => {
-  return setToStorage({ styles }).then(response => response.styles);
-};
